ba5fa70ad2a6f8dff2949c59c0ac916cdacbdaa1
Hans Barredo <hansit.barredo@gmail.com>
2025-05-15 09:42:06 +0800

    product js edit

--- src/js/product.js
+++ src/js/product.js
@@ -5,20 +5,18 @@ import ProductDetails from "./ProductDetails.mjs";
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-<<<<<<< HEAD
 function addProductToCart(product) {
-<<<<<<< HEAD
   let products = getLocalStorage("so-cart");
   if (!Array.isArray(products)) {
     products = [];
   }
   products.push(product);
   setLocalStorage("so-cart", products);
-=======
+
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
->>>>>>> abe33442832fcb1ea279179e16a5f2117d8fc558
+
 }
 
 // add to cart button event handler
@@ -31,7 +29,7 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-=======
+  
 const product = new ProductDetails(productId, dataSource);
 product.init();
->>>>>>> d10d3b55d9bbc5dc00366ff8e498c8988c225094
+
