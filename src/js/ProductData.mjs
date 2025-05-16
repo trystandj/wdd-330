0a43248ca0224ce2a3c99b466a950c61c177edc8
migarcos <migark@hotmail.com>
2025-05-16 15:27:28 -0500

    changed to w02 team activity

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -11,13 +11,17 @@ export default class ProductData {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
+  // The getData() method uses just promises and .then()
+  getData() {   
     return fetch(this.path)
       .then(convertToJson)
+      //    function(data) { return data; }
       .then((data) => data);
   }
+  // findProductById() method uses async/await. 
   async findProductById(id) {
     const products = await this.getData();
+    //                   function(item) { return item.Id === id; }
     return products.find((item) => item.Id === id);
   }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,40 +1,39 @@
 //  *  1st W Changed  *   // import { setLocalStorage } from "./utils.mjs"; 
-import { getLocalStorage , setLocalStorage } from "./utils.mjs";
+// import { getLocalStorage , setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("Product");
 
-function addProductToCart(product) {
-  // next line is the original
-  // setLocalStorage("so-cart", product);
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-  let cart = getLocalStorage("so-cart");
+// console.log(dataSource.findProductById(productId));
 
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-  // const inCarProd = cart.find((item) => item.Id === product.Id);
+// function addProductToCart(product) {
+//   // next line is the original
+//   // setLocalStorage("so-cart", product);
 
-  // if (inCarProd) {
-  //   inCarProd.Quantity += 1;
-  // } else {
-  //   product.Quantity = 1;
+//   let cart = getLocalStorage("so-cart");
 
-  //   cart.push(product);
-  // }
+//   if (!Array.isArray(cart)) {
+//     cart = [];
 
-  cart.push(product); // add individual products 
+//     cart.push(product); // add individual products 
 
-  setLocalStorage("so-cart", cart);
-}
+//     setLocalStorage("so-cart", cart);
+//   }
+// }
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
