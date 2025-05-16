2a2408d5a237323662acef6837c1237b98d6905f
trystandj <trystandj@gmail.com>
2025-05-15 21:17:25 -0400

    Finished W02 Individual Activity: Dynamic Product list and fixed a few issues

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,6 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,14 @@ export default defineConfig({
   root: "src/",
 
   build: {
-  outDir: "../dist",
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, "src/index.html"),
-      cart: resolve(__dirname, "src/cart/index.html"),
-      checkout: resolve(__dirname, "src/checkout/index.html"),
-      product: resolve(__dirname, "src/product_pages/index.html"
-      ),
-
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
     },
   },
-},
 });
