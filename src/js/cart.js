3c4a361535335d556634d52db177b703abe8b039
migarcos <migark@hotmail.com>
2025-05-17 12:29:32 -0500

    w02 individual working

--- src/js/cart.js
+++ src/js/cart.js
@@ -10,7 +10,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="../public/images/tents/${item.Image}"
       alt="${item.Name}"
     />
   </a>
