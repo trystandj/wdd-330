773de1a1cd68762b472a980c22e70df6bd42a39d
migarcos <migark@hotmail.com>
2025-05-17 08:31:27 -0500

    w02 individual changes

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,33 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+                <a href="product_pages/?product=">
+                    <img src="" alt="Image of">
+                    <h2 class="card__brand"></h2>
+                    <h3 class="card__name"></h3>
+                    <p class="product-name__price">$</p>
+                </a>
+            </li>`;
+}
+
+export default class ProductList {
+
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        // const htmlStrings = list.map(productCardTemplate);
+        // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(''));
+
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+}
