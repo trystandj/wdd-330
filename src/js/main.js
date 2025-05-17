cafc92fbe771902dbb5d2b34428f0526b0af7751
Hans Barredo <hansit.barredo@gmail.com>
2025-05-17 11:16:40 +0800

    run lint and format

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-
 const dataSource = new ProductData("tents");
-console.log(dataSource)
 
-const element = document.querySelector(".product-list")
+const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
-
 
 productList.init();
