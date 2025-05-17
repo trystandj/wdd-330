d7beeda8ca522989fb9f198c5e7d78b9fe007f54
Hans Barredo <hansit.barredo@gmail.com>
2025-05-17 16:51:01 +0800

    fixed rules warnings

--- src/js/alert.js
+++ src/js/alert.js
@@ -5,22 +5,14 @@ export default class Alert {
   }
 
   async loadAlerts() {
-    try {
       const response = await fetch(this.jsonUrl);
       if (!response.ok) throw new Error("Failed to load alerts");
       const alerts = await response.json();
       if (alerts.length > 0) this.renderAlerts(alerts);
-    } catch (error) {
-      console.error("Error loading alerts:", error);
-    }
   }
 
   renderAlerts(alerts) {
     const main = document.querySelector("main");
-    if (!main) {
-      console.warn("<main> element not found in the document.");
-      return;
-    }
 
     const section = document.createElement("section");
     section.className = this.containerClass;
