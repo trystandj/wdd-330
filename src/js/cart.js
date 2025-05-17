f6779979c2c8418615a085d54b25933d5636b0d0
migarcos <migark@hotmail.com>
2025-05-17 17:21:29 -0500

    Total to Cart was added and work

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,23 +2,23 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
-  if ( cartItems.length > 0) {
+  // ( cartItems.length > 0)
+  if (cartItems) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
   }
 }
 
 function calcTotalCart() {
-  const cartItems = getLocalStorage("so-cart") || [];
-  if ( cartItems.length == 0) {
-    document.querySelector(".hide").classList.add("hide");
+  const cartItems = getLocalStorage("so-cart");
+  if ( !cartItems) {
+    document.querySelector("#cart-footer").classList.add("hide");
   } else {
     document.querySelector(".hide").classList.remove("hide");
     let total = cartItems.reduce( (sum, item) => sum + item.ListPrice, 0);
     // console.log(total);  
     document.querySelector("#cart-calc").innerHTML = total;
-  }
-  
+  } 
 }
 
 function cartItemTemplate(item) {
