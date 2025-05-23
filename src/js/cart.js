ae14d3af986bb43b4679546e1bec94045ad6c310
Hans Barredo <hansit.barredo@gmail.com>
2025-05-23 16:24:30 +0800

    formatted code

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 const cartElement = document.getElementById("cart-items");
-import {getNumberOfItems, loadHeaderFooter} from "./utils.mjs";
+import { getNumberOfItems, loadHeaderFooter } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -66,4 +66,3 @@ function removeProductFromCart(index) {
 renderCartContents();
 loadHeaderFooter();
 calcTotalCart();
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import {loadHeaderFooter} from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import Alert from "./alert.js";
-
 
 const alert = new Alert();
 alert.loadAlerts();
