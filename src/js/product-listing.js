6f6b001fb1f918f0a5399482d7715c01750e6e8d
Hans Barredo <hansit.barredo@gmail.com>
2025-05-24 08:19:56 +0800

    Expand Inventory activity

---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
+
+productList.init();
+
+loadHeaderFooter();
