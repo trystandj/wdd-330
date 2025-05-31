8c76d4e3e6b5131b8da187efdd7d7a6c1a98982b
trystandj <trystandj@gmail.com>
2025-05-30 21:04:16 -0400

    Registration task has been setup

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -40,4 +40,18 @@ export default class ExternalServices {
     };
     return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
   }
+  
+  async createUser(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(`${baseURL}users/`, options).then(convertToJson);
+  }
 }
+
+
+
